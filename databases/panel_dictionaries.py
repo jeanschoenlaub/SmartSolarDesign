794d85d19228708545040d39d5d401522fd70081
jeanschoenlaub <jeanschoen@hotmail.com>
2021-02-15 12:25:15 +1100

    Finished pdf reader

--- databases/panel_dictionaries.py
+++ databases/panel_dictionaries.py
@@ -1,14 +1,14 @@
 lg_dict = {
-"LG330NB": {"P":"330", "Voc": "40.6", "Isc":"10.30","model":"LG NeON 2 BiFacial","Vmp":"33.8","Url":"https://www.lgenergy.com.au/uploads/download_files/0553d2da4e98b8c198b0da413f9449fbdddace94.pdf"},
-"LG355N": {"P":"355", "Voc": "41.4", "Isc":"10.65","model":"LG NeON 2","Vmp":"35.7","Url":"https://www.lgenergy.com.au/uploads/download_files/075fed5145c7b387f78464077ec7f68d158a7950.pdf"},
-"LG360N": {"P":"360", "Voc": "41.6", "Isc":"10.84","model":"LG NeON 2","Vmp":"35.1","Url":"https://www.lgenergy.com.au/uploads/download_files/075fed5145c7b387f78464077ec7f68d158a7950.pdf"},
-"LG365N": {"P":"365", "Voc": "41.7", "Isc":"10.88","model":"LG NeON 2","Vmp":"35.5","Url":"https://www.lgenergy.com.au/uploads/download_files/c368443778f812ed588ab828f94d48665c29a89f.pdf"},
-"LG365M": {"P":"365", "Voc": "41.57", "Isc":"11.22","model":"LG Mono X Plus","Vmp":"33.82","Url":"https://www.lgenergy.com.au/uploads/download_files/46110da0092b6cdf8feb6e84097201751420b38a.pdf"},
-"LG370M": {"P":"370", "Voc": "41.72", "Isc":"11.32","model":"LG Mono X Plus","Vmp":"33.95","Url":"https://www.lgenergy.com.au/uploads/download_files/46110da0092b6cdf8feb6e84097201751420b38a.pdf"}
+"LG330NB": {"P":"330", "Voc": "40.6", "Isc":"10.30","model":"LG NeON 2 BiFacial","Vmp":"33.8","panelSerial":"","Url":"https://www.lgenergy.com.au/uploads/download_files/0553d2da4e98b8c198b0da413f9449fbdddace94.pdf"},
+"LG355N": {"P":"355", "Voc": "41.4", "Isc":"10.65","model":"LG NeON 2","Vmp":"35.7","panelSerial":"","Url":"https://www.lgenergy.com.au/uploads/download_files/075fed5145c7b387f78464077ec7f68d158a7950.pdf"},
+"LG360N": {"P":"360", "Voc": "41.6", "Isc":"10.84","model":"LG NeON 2","Vmp":"35.1","panelSerial":"","Url":"https://www.lgenergy.com.au/uploads/download_files/075fed5145c7b387f78464077ec7f68d158a7950.pdf"},
+"LG365N": {"P":"365", "Voc": "41.7", "Isc":"10.88","model":"LG NeON 2","Vmp":"35.5","panelSerial":"","Url":"https://www.lgenergy.com.au/uploads/download_files/c368443778f812ed588ab828f94d48665c29a89f.pdf"},
+"LG365M": {"P":"365", "Voc": "41.57", "Isc":"11.22","model":"LG Mono X Plus","Vmp":"33.82","panelSerial":"","Url":"https://www.lgenergy.com.au/uploads/download_files/46110da0092b6cdf8feb6e84097201751420b38a.pdf"},
+"LG370M": {"P":"370", "Voc": "41.72", "Isc":"11.32","model":"LG Mono X Plus","Vmp":"33.95","panelSerial":"LG370S1C-U6","Url":"https://www.lgenergy.com.au/uploads/download_files/46110da0092b6cdf8feb6e84097201751420b38a.pdf"}
 }
 
 qcells_dict = {
-"QMAXX350": {"P":"350", "Voc": "40.7", "Isc":"10.74","model":"Q.MAXX-G2","Vmp":"34.24","Url":"https://www.q-cells.com/au/main/customer_support/download/datasheets.html"}
+"QMAXX350": {"P":"350", "Voc": "40.7", "Isc":"10.74","model":"Q.MAXX-G2","Vmp":"34.24","panelSerial":"","Url":"https://www.q-cells.com/au/main/customer_support/download/datasheets.html"}
 }
 
 panel_dict = {
